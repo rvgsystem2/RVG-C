8f99e112a0ae2ca836102766af538088bedc5934
chandan <chandandigital6@gmail.com>
2025-07-01 10:56:06 +0530

    home controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\BannerController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProfileController;
@@ -24,14 +25,14 @@ use Illuminate\Support\Facades\Route;
 
 
 // frontend routes
-Route::get('/', HomeController::class, 'index')->name('index');
-Route::get('/about', HomeController::class, 'about')->name('about');
-Route::get('service', HomeController::class, 'service')->name('service');
-Route::get('/team', HOmeController::class, 'team')->name('team');
-Route::get('/testimonial', HomeController::class, 'testimonial')->name('testimonial');
-Route::get('/contact', HomeController::class, 'contact')->name('contact');
-Route::get('/404', HomeController::class, 'notFound')->name('404');
-Route::get('/project', HomeController::class, 'project')->name('project');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/service', [HomeController::class, 'service'])->name('service');
+Route::get('/team', [HomeController::class, 'team'])->name('team');
+Route::get('/testimonial', [HomeController::class, 'testimonial'])->name('testimonial');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/404', [HomeController::class, 'notFound'])->name('404');
+Route::get('/project', [HomeController::class, 'project'])->name('project');
 
 
 
@@ -80,6 +81,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/user/permissions/{user}', [UserController::class, 'assignPermissionForm'])->name('user.permission.form');
     Route::post('/user/permissions/{user}', [UserController::class, 'assignPermissionToUser'])->name('user.assign-permission');
 
+
+   // banner routes
+    Route::get('banner/index',[BannerController::class,'index'])->name('banner.index');
+    Route::get('banner/create',[BannerController::class,'create'])->name('banner.create');
+    Route::post('banner/store',[BannerController::class,'store'])->name('banner.store');
+    Route::get('banner/edit/{banner}',[BannerController::class,'edit'])->name('banner.edit');
+    Route::post('banner/update/{banner}',[BannerController::class,'update'])->name('banner.update');
+    Route::get('banner/delete/{banner}',[BannerController::class,'delete'])->name('banner.delete');
 
 
 
