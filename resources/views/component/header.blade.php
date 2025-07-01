6ed011a7ebb862faa70efa9d2307aa06d2d37795
rvgsystem2 <rvgsystem2@gmail.com>
2025-07-01 10:21:16 +0530

    sr

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0 ">
     <a href="" class="navbar-brand p-0">
-        <h1 class="m-0">DGital</h1>
+        {{-- <h1 class="m-0">DGital</h1> --}}
+        <img src="{{asset('logo.png')}}" alt="" class="logo">
+        {{-- <img src="{{asset('asset/img/logo-w.png')}}" alt="" class="logo" > --}}
         <!-- <img src="img/logo.png" alt="Logo"> -->
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -12,16 +14,16 @@
             <a href="about.html" class="nav-item nav-link">About</a>
             <a href="service.html" class="nav-item nav-link">Service</a>
             <a href="project.html" class="nav-item nav-link">Project</a>
-            <div class="nav-item dropdown">
+            {{-- <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu m-0">
                     <a href="team.html" class="dropdown-item">Our Team</a>
                     <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                     <a href="404.html" class="dropdown-item">404 Page</a>
                 </div>
-            </div>
+            </div> --}}
             <a href="contact.html" class="nav-item nav-link">Contact</a>
         </div>
-        <a href="#" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get Started</a>
+        <a href="#" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block text-danger">Get Started</a>
     </div>
 </nav>
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -23,7 +23,7 @@
                         <h1 class="text-white mb-4 animated slideInDown">A Digital Agency Of Inteligents & Creative People</h1>
                         <p class="text-white pb-3 animated slideInDown">Tempor rebum no at dolore lorem clita rebum rebum ipsum rebum stet dolor sed justo kasd. Ut dolor sed magna dolor sea diam. Sit diam sit justo amet ipsum vero ipsum clita lorem</p>
                         <a href="" class="btn btn-secondary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft">Read More</a>
-                        <a href="" class="btn btn-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">Contact Us</a>
+                        <a href="" class="btn btn-light py-sm-3 px-sm-5 rounded-pill animated slideInRight text-danger">Contact Us</a>
                     </div>
                     <div class="col-lg-6 text-center text-lg-start pt-5">
                         <img class="img-fluid animated zoomIn" src="{{asset('front-asset/img/hero.png')}}" alt="">
