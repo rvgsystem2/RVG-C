a047781fded7ae0ada8c68d62399ae8b1dca7f47
rvgsystem2 <rvgsystem2@gmail.com>
2025-07-02 11:16:30 +0530

    fotter

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,83 +1,69 @@
-    <!-- Footer Start -->
-    <div class="container-fluid custom-color text-light wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5 px-lg-5">
-            <div class="row g-5">
-                <div class="col-md-6 col-lg-3">
-                    <p class="section-title text-white h5 mb-4">Address<span></span></p>
-                    <p><i class="fa fa-map-marker-alt me-3"></i>  73 Basement, Ekta Enclave Society, Lakhanpur, Khyora, Kanpur, Uttar Pradesh 208024</p>
-                    <p><i class="fa fa-phone-alt me-3"></i>+918299012292</p>
-                    <p><i class="fa fa-envelope me-3"></i>realvictorygroups.com</p>
-                    <div class="d-flex pt-2 footer-social">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-instagram"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <p class="section-title text-white h5 mb-4">Quick Link<span></span></p>
-                    <a class="btn btn-link" href="{{route('about')}}">About Us</a>
-                    <a class="btn btn-link" href="{{route('contact')}}">Contact Us</a>
-                    <a class="btn btn-link" href="">Privacy Policy</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">Career</a>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <p class="section-title text-white h5 mb-4">Our Services<span></span></p>
-                    <div class="row g-2">
-                        <div class="col-4">
-                            <a class="btn btn-link" href="">Digital Marketing</a>
-                        </div>
-                        <div class="col-4">
-                            <a class="btn btn-link" href="">Social Media Handling</a>
-                        </div>
-                        <div class="col-4">
-                            <a class="btn btn-link" href="">Web Development</a>
-                        </div>
-                        <div class="col-4">
-                            <a class="btn btn-link" href="">App Development</a>
-                        </div>
-                        <div class="col-4">
-                            <a class="btn btn-link" href=""></a>
-                        </div>
-                        <div class="col-4">
-                            <a class="btn btn-link" href="">Web Development</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <p class="section-title text-white h5 mb-4">Newsletter<span></span></p>
-                    <p>Lorem ipsum dolor sit amet elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulpu</p>
-                    <div class="position-relative w-100 mt-3">
-                        <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="text" placeholder="Your Email" style="height: 48px;">
-                        <button type="button" class="btn btn-send shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane fs-4"></i></button>
-                    </div>
+<!-- Footer Start -->
+<div class="container-fluid custom-color text-light wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5 px-lg-5">
+        <div class="row gy-5 gx-4">
+            <!-- Contact Info -->
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-white mb-4">Address</h5>
+                <p><i class="fa fa-map-marker-alt me-3"></i>73 Basement, Ekta Enclave Society, Lakhanpur, Khyora, Kanpur, UP 208024</p>
+                <p><i class="fa fa-phone-alt me-3"></i>+91 82990 12292</p>
+                <p><i class="fa fa-envelope me-3"></i>info@realvictorygroups.com</p>
+                <div class="d-flex pt-3">
+                    <a class="btn btn-outline-light btn-social me-2" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social me-2" href="#"><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
-        </div>
-        <div class="container px-lg-5">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
 
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                            <a href="">Home</a>
-                            <a href="">Cookies</a>
-                            <a href="">Help</a>
-                            <a href="">FQAs</a>
-                        </div>
-                    </div>
+            <!-- Quick Links -->
+            <div class="col-lg-2 col-md-6">
+                <h5 class="text-white mb-4">Quick Links</h5>
+                <a class="btn btn-link text-light" href="{{route('about')}}">About Us</a>
+                <a class="btn btn-link text-light" href="{{route('contact')}}">Contact Us</a>
+                <a class="btn btn-link text-light" href="#">Privacy Policy</a>
+                <a class="btn btn-link text-light" href="#">Terms & Conditions</a>
+                <a class="btn btn-link text-light" href="#">Career</a>
+            </div>
+
+            <!-- Our Services -->
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white mb-4">Our Services</h5>
+                <a class="btn btn-link text-light" href="#">Digital Marketing</a>
+                <a class="btn btn-link text-light" href="#">Social Media Handling</a>
+                <a class="btn btn-link text-light" href="#">Web Development</a>
+                <a class="btn btn-link text-light" href="#">App Development</a>
+                <a class="btn btn-link text-light" href="#">Wordpress website</a>
+            </div>
+
+            <!-- Newsletter -->
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white mb-4">Newsletter</h5>
+                <p>Subscribe to receive latest news and updates from us.</p>
+                <div class="position-relative w-100 mt-3">
+                    <input class="form-control border-0 rounded-pill ps-4 pe-5" type="email" placeholder="Your Email" style="height: 48px;">
+                    <button type="submit" class="btn btn-send shadow-none position-absolute top-0 end-0 mt-1 me-2">
+                        <i class="fa fa-paper-plane fs-5"></i>
+                    </button>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer End -->
 
-   <!-- Back to Top -->
+    <!-- Copyright -->
+    <div class="container px-lg-5 border-top border-light pt-4 mt-4">
+        <div class="row align-items-center">
+            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                &copy; <a class="text-white fw-bold" href="#">2025 Real Victory Groups</a>. All Rights Reserved.
 
-   <a href="#" class="btn btn-lg btn-dark btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-dark btn-lg-square back-to-top">
+    <i class="bi bi-arrow-up"></i>
+</a>
