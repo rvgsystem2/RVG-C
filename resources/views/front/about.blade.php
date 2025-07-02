0724e6c2b3d89776cde8bc382a043ce9c9bf546e
rvgsystem2 <rvgsystem2@gmail.com>
2025-07-02 13:51:40 +0530

    arE

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -1,33 +1,33 @@
 @extends('component.main')
 @section('content')
-
-<!-- Page Header -->
-<div class="container-fluid custom-color py-5">
-    <div class="container text-center py-5">
-        <h1 class="display-3 text-white mb-3 animated slideInDown">About Us</h1>
-        <nav aria-label="breadcrumb" class="animated slideInDown">
-            <ol class="breadcrumb justify-content-center mb-0">
-                <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                <li class="breadcrumb-item text-white active" aria-current="page">About Us</li>
-            </ol>
-        </nav>
+    <!-- Page Header -->
+    <div class="container-fluid custom-color py-5">
+        <div class="container text-center py-5">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">About Us</h1>
+            <nav aria-label="breadcrumb" class="animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                    <li class="breadcrumb-item text-white active" aria-current="page">About Us</li>
+                </ol>
+            </nav>
+        </div>
     </div>
-</div>
 
-<!-- Features -->
-@include('front.product')
+    <!-- Features -->
+    @include('front.product')
 
-<!-- About Section -->
- <div class="container-fluid py-5">
-            @forelse ($abouts as $about)
-                <div class="container py-5 px-lg-5">
+    <!-- About Section -->
+    <div class="container-fluid py-5">
+        @forelse ($abouts as $about)
+            <div class="container py-5 px-lg-5">
                 <p class="section-title ">{{ $about->title }}<span></span></p>
                 <div class="row g-5 align-items-center">
 
                     <!-- Image Column: Order-1 on mobile, Order-2 on large screens -->
                     <div class="col-lg-6 order-1 order-lg-2">
                         <img class="img-fluid wow zoomIn" data-wow-delay="0.5s"
-                            src="{{ asset('storage/' . ($about->image ?? 'front-asset/img/about.png')) }}" alt="{{ $about->image_alt ?? 'Abouts Image' }}">
+                            src="{{ asset('storage/' . ($about->image ?? 'front-asset/img/about.png')) }}"
+                            alt="{{ $about->image_alt ?? 'Abouts Image' }}">
                     </div>
 
                     <!-- Content Column: Order-2 on mobile, Order-1 on large screens -->
@@ -43,34 +43,39 @@
 
                 </div>
             </div>
-            @empty
+        @empty
+        @endforelse
+    </div>
 
-            @endforelse
-        </div>
-<!-- Facts -->
-<section class="custom-color py-5 text-white">
-    <div class="container">
-        <div class="row text-center g-4">
-            <div class="col-md-3">
-                <i class="fa fa-certificate fa-2x mb-2"></i>
-                <h3>10+</h3><p>Years Experience</p>
-            </div>
-            <div class="col-md-3">
-                <i class="fa fa-users-cog fa-2x mb-2"></i>
-                <h3>25</h3><p>Team Members</p>
-            </div>
-            <div class="col-md-3">
-                <i class="fa fa-users fa-2x mb-2"></i>
-                <h3>500+</h3><p>Satisfied Clients</p>
-            </div>
-            <div class="col-md-3">
-                <i class="fa fa-check fa-2x mb-2"></i>
-                <h3>850+</h3><p>Projects Completed</p>
+    <!-- Facts Start -->
+    <div class="container-fluid custom-color py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5 px-lg-5">
+            <div class="row g-4">
+                <div class="col-6 col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
+                    <i class="fa fa-certificate fa-3x mb-3"></i>
+                    <h1 class="text-white mb-2" data-toggle="counter-up">1234</h1>
+                    <p class="text-white mb-0">Years Experience</p>
+                </div>
+                <div class="col-6 col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
+                    <i class="fa fa-users-cog fa-3x  mb-3"></i>
+                    <h1 class="text-white mb-2" data-toggle="counter-up">1234</h1>
+                    <p class="text-white mb-0">Team Members</p>
+                </div>
+                <div class="col-6 col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
+                    <i class="fa fa-users fa-3x  mb-3"></i>
+                    <h1 class="text-white mb-2" data-toggle="counter-up">1234</h1>
+                    <p class="text-white mb-0">Satisfied Clients</p>
+                </div>
+                <div class="col-6 col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
+                    <i class="fa fa-check fa-3x  mb-3"></i>
+                    <h1 class="text-white mb-2" data-toggle="counter-up">1234</h1>
+                    <p class="text-white mb-0">Complete Projects</p>
+                </div>
             </div>
         </div>
     </div>
-</section>
 
+    <!-- Facts End -->
 
     <!-- Team Start -->
     <div class="container-flued py-5">
@@ -83,7 +88,8 @@
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item bg-custom rounded">
                         <div class="text-center border-bottom p-4">
-                            <img class="img-fluid rounded-circle mb-4" src="{{asset('front-asset/img/team-1.jpg')}}" alt="">
+                            <img class="img-fluid rounded-circle mb-4" src="{{ asset('front-asset/img/team-1.jpg') }}"
+                                alt="">
                             <h5>John Doe</h5>
                             <span>CEO & Founder</span>
                         </div>
@@ -98,7 +104,8 @@
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-custom rounded">
                         <div class="text-center border-bottom p-4">
-                            <img class="img-fluid rounded-circle mb-4" src="{{asset('front-asset/img/team-2.jpg')}}" alt="">
+                            <img class="img-fluid rounded-circle mb-4" src="{{ asset('front-asset/img/team-2.jpg') }}"
+                                alt="">
                             <h5>Jessica Brown</h5>
                             <span>Web Designer</span>
                         </div>
@@ -113,7 +120,8 @@
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="team-item bg-custom rounded">
                         <div class="text-center border-bottom p-4">
-                            <img class="img-fluid rounded-circle mb-4" src="{{asset('front-asset/img/team-3.jpg')}}" alt="">
+                            <img class="img-fluid rounded-circle mb-4" src="{{ asset('front-asset/img/team-3.jpg') }}"
+                                alt="">
                             <h5>Tony Johnson</h5>
                             <span>SEO Expert</span>
                         </div>
@@ -129,5 +137,4 @@
         </div>
     </div>
     <!-- Team End -->
-
 @endsection
