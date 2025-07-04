ea217751748a80e131c4da70f7c95d50c5daca6e
chandan <chandandigital6@gmail.com>
2025-07-04 16:19:29 +0530

    about

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -74,7 +74,7 @@
                         <div class="p-4">
                             <h5 class="mb-1">{{ $team->name }}</h5>
                             <p class="text-muted small mb-2">{{ $team->designation }}</p>
-                            <p class="text-muted small mb-3">{{ $team->description }}</p>
+                            <p class="text-muted small mb-3">{{ $team->message }}</p>
                             @if ($team->company)
                                 <p class="text-muted small fst-italic">{{ $team->company }}</p>
                             @endif
