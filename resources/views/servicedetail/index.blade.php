810bd1a6b2535b23f19f9c659ee36cde23e63397
chandandigital6 <chandandigital6@gmail.com>
2026-04-07 10:34:35 +0530

    serice

--- resources/views/servicedetail/index.blade.php
+++ resources/views/servicedetail/index.blade.php
@@ -51,11 +51,11 @@
                                             </a>
 
                                             <form action="{{ route('service-detail.delete', $detail->id) }}"
-                                                method="POST"
+                                                method="get"
                                                 onsubmit="deleteServiceDetail(event)"
                                                 class="inline-block">
                                                 @csrf
-                                                @method('DELETE')
+                                                {{-- @method('DELETE') --}}
                                                 <button type="submit"
                                                     class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md text-sm font-semibold transition shadow">
                                                     🗑️ Delete
